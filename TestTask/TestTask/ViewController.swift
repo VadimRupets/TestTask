4a3642e1d5357043b5450b0345092da084451f2d
Vadim Rupets <vadzim.rupets@itechart-group.com>
2018-03-26 14:31:06 +0300

    Created Xcode project

---
+++ TestTask/TestTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Vadim Rupets on 3/26/18.
+//  Copyright © 2018 Vadim Rupets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
